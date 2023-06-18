339dd6e8ffbd88b17da60098a074de2d79739e3d
Jason <jason5donoghue@gmail.com>
2023-06-18 16:05:34 +0100

    cleaned up files, ready for merge

--- my_profile_page/src/Header.js
+++ my_profile_page/src/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="header">
-            <h1>My Profile Page</h1>
+            <h1>Jason Donoghue Profile Page</h1>
         </div>
     )
 }
